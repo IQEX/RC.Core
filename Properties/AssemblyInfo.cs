5c95eb7db57aa1ccd0f0071208c9db3c417d48a0
Yuuki Wesp <ls-micro@ya.ru>
2015-10-04 18:08:42 +0300

    Update Version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eris")]
 [assembly: AssemblyProduct("RC.Core")]
-[assembly: AssemblyCopyright("Copyright © Of Fire Twins Wesp 2015")]
+[assembly: AssemblyCopyright("Copyright © Yuuki Wesp 2015")]
 [assembly: AssemblyTrademark("RC")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.5.0.0")]
-[assembly: AssemblyFileVersion("7.5.0.0")]
+[assembly: AssemblyVersion("7.7.0.0")]
+[assembly: AssemblyFileVersion("7.7.0.0")]
 [assembly: InternalsVisibleTo("AppUnitTest, PublicKey=002400000480000094000000060200000024000052534131000400000100010039e50bf6193da3" +
                                                   "93d5cf03d6abd2363f45f1488a851109ff0a9185e3a83d7fce98c133945e4775877e0c48a7c9ce" +
                                                   "3fde4b0d2480aa60d65ab2c1247c2e3c37aab23b72ea9f386a83fe364f3c43c46f34abc2939420" +
