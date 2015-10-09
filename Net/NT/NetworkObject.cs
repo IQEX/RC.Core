bb0d1604f3f9a341b0957b22231300b8b3d5697a
Yuuki Wesp <ls-micro@ya.ru>
2015-10-10 02:10:16 +0300

    Add NT network object

---
+++ Net/NT/NetworkObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rc.Framework.Net.NTCore
+{
+    /// <summary>
+    /// Class of Network Object
+    /// </summary>
+    public abstract class NetworkObject
+    {
+        /// <summary>
+        /// Class to Byte Array
+        /// </summary>
+        /// <returns></returns>
+        public abstract byte[] ToByte();
+        /// <summary>
+        /// Byte Array To Content Class
+        /// </summary>
+        /// <param name="bitBox">Content</param>
+        public abstract void outByte(byte[] bitBox);
+    }
+}
