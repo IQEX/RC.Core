f68450b317f486cd6198e8b6c032a52aa92d78a8
Yuuki Wesp <ls-micro@ya.ru>
2015-10-10 03:10:44 +0300

    Fix #ArrayWMIResponce, add Query

--- Native/WMI.cs
+++ Native/WMI.cs
@@ -13,30 +13,41 @@ namespace Rc.Framework.Native
     public class WMI
     {
         /// <summary>
-        /// Version Win32
+        /// Info System Win32
         /// </summary>
-        public const string SelectVersionWindows = "SELECT Version FROM Win32_OperatingSystem";
+        public const string QuerySystemWindows = "SELECT * FROM Win32_OperatingSystem";
+        /// <summary>
+        /// Info Base Board
+        /// </summary>
+        public const string QueryBaseBoard = "SELECT * FROM Win32_BaseBoard";
+        /// <summary>
+        /// Info Video Adapter
+        /// </summary>
+        public const string QueryVideoAdapter = "SELECT * FROM Win32_VideoController";
+        /// <summary>
+        /// Info Network Adapter
+        /// </summary>
+        public const string QueryNetworkAdapter = "SELECT * FROM Win32_NetworkAdapter";
         /// <summary>
         /// Query method
         /// </summary>
         /// <param name="qu">Query</param>
         /// <param name="xBase">result</param>
         /// <returns></returns>
-        public bool Query(string qu, out Dictionary<String, String> xBase)
+        public bool Query(string qu, out List<Dictionary<String, String>> xBase)
         {
-            xBase = new Dictionary<string, string>();
+            xBase = new List<Dictionary<string, string>>();
             try
             {
-
                 var mng = new ManagementObjectSearcher(qu);
                 foreach (ManagementObject ob in mng.Get())
                 {
-                    foreach(PropertyData dta in ob.Properties)
+                    xBase.Add(new Dictionary<string, string>());
+                    foreach (PropertyData dta in ob.Properties)
                     {
-                        xBase.Add(dta.Name, (string)dta.Value);
+                        xBase[xBase.Count - 1].Add(dta.Name, (string)dta.Value);
                     }
                 }
-
                 return true;
             }
             catch
