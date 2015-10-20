51e03617c48b39296664b2d9e8c47e53188fd5c3
Yuuki Wesp <ls-micro@ya.ru>
2015-10-20 19:48:34 +0300

    Framework 8.0 release

--- Framework.cs
+++ Framework.cs
@@ -2,6 +2,10 @@
 // License="root\\LICENSE"              //   Copyright © Of Fire Twins Wesp 2015  <ls-micro@ya.ru>      //
 // LicenseType="MIT"                    //                  Alise Wesp & Yuuki Wesp                     //
 // =====================================//==============================================================//
+using System;
+using System.IO;
+using System.Reflection;
+
 namespace Rc.Framework
 {
     /// <summary>
@@ -12,6 +16,47 @@ namespace Rc.Framework
         /// <summary>
         /// Версия
         /// </summary>
-        public readonly static string Version = "Live-7.7 CAT #S";
+        public readonly static string Version = $"{GetInfoFramework().ver.Major}{GetInfoFramework().ver.Minor}{GetInfoFramework().ver.Build}";
+        /// <summary>
+        /// Получение информации о сборке фреймворка
+        /// </summary>
+        /// <returns></returns>
+        public static RCAssemblyInfo GetInfoFramework()
+        {
+            Assembly asm = AppDomain.CurrentDomain.Load(File.ReadAllBytes("Rc.Core.dll"));
+            RCAssemblyInfo rv = new RCAssemblyInfo();
+            AssemblyName name = asm.GetName();
+            rv.ver = name.Version;
+            rv.CodeBase = name.CodeBase;
+            rv.Name = name.FullName;
+            rv.ProcArch = name.ProcessorArchitecture;
+            return rv;
+        }
+    }
+}
+
+namespace Rc
+{
+    /// <summary>
+    /// class of version framework to rc line
+    /// </summary>
+    public class RCAssemblyInfo
+    {
+        /// <summary>
+        /// Version
+        /// </summary>
+        public Version ver;
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// CodeBase
+        /// </summary>
+        public string CodeBase;
+        /// <summary>
+        /// ProcessorArchitecture
+        /// </summary>
+        public ProcessorArchitecture ProcArch;
     }
 }
