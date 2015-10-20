930ffb28ce0fab13e2d572b5c2d74914ed446a55
Yuuki Wesp <ls-micro@ya.ru>
2015-10-20 15:48:09 +0300

    add manipulate a pointer to a stream

---
+++ IO/StreamPtr.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rc.Framework.IO
+{
+    /// <summary>
+	/// Manipulate a pointer to a stream.
+	/// </summary>
+	public struct StreamPtr
+    {
+        Stream stream;
+        long offset;
+
+        public StreamPtr(Stream s)
+            : this(s, s.Position)
+        {
+        }
+        public StreamPtr(Stream s, long pos)
+        {
+            if (s == null)
+                throw new ArgumentNullException("stream");
+            stream = s;
+            offset = pos;
+        }
+
+        public static StreamPtr operator +(StreamPtr sp, long plus) { return new StreamPtr(sp.stream, sp.offset + plus); }
+        public static StreamPtr operator -(StreamPtr sp, long plus) { return new StreamPtr(sp.stream, sp.offset - plus); }
+
+        public static long operator -(StreamPtr sp, StreamPtr sp2)
+        {
+            if (sp.stream != sp2.stream)
+                throw new ArgumentException();
+            return sp.offset - sp2.offset;
+        }
+
+        public static explicit operator Stream(StreamPtr ptr)
+        {
+            ptr.stream.Position = ptr.offset;
+            return ptr.stream;
+        }
+    }
+}
