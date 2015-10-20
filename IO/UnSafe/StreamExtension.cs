a4ba6460765c98d0922540fc1d71bbba7ed71c4a
Yuuki Wesp <ls-micro@ya.ru>
2015-10-20 15:57:49 +0300

    add stream extension

---
+++ IO/UnSafe/StreamExtension.cs
@@ -0,0 +1,27 @@
+﻿// =====================================//==============================================================//
+// License="root\\LICENSE"              //   Copyright © Of Fire Twins Wesp 2015  <ls-micro@ya.ru>      //
+// LicenseType="MIT"                    //                  Alise Wesp & Yuuki Wesp                     //
+// =====================================//==============================================================//
+namespace Rc.Framework.IO.UnSafe
+{
+    /// <summary>
+    /// Extension to Stream
+    /// </summary>
+    public static class StreamExtension
+    {
+        /// <summary>
+        /// Read a structure in stream
+        /// </summary>
+        /// <typeparam name="T">structure</typeparam>
+        /// <param name="s"></param>
+        /// <returns>structure</returns>
+        public unsafe static T Read<T>(this System.IO.Stream s) where T : struct
+        {
+            int n = System.Runtime.InteropServices.Marshal.SizeOf(typeof(T));
+            var buf = new byte[n];
+            s.Read(buf, 0, n);
+            fixed (byte* pbuf = &buf[0])
+                return (T)System.Runtime.InteropServices.Marshal.PtrToStructure((System.IntPtr)pbuf, typeof(T));
+        }
+    }
+}
