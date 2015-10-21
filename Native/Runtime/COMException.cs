4e1931fd21f6fedb6705710ce94db5ec02e0987b
Yuuki Wesp <ls-micro@ya.ru>
2015-10-21 14:43:38 +0300

    add untime com

---
+++ Native/Runtime/COMException.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Globalization;
+namespace Rc.Framework.Native.Runtime
+{
+    /// <summary>
+    ///   The base class for errors that occur in COM.
+    /// </summary>
+    public class COMException : Exception
+    {
+        private ResultDescriptor descriptor;
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "COMException" /> class.
+        /// </summary>
+        public COMException() : base("A COM exception occurred.")
+        {
+            this.descriptor = ResultDescriptor.Find(Result.Fail);
+            HResult = (int)Result.Fail;
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "COMException" /> class.
+        /// </summary>
+        /// <param name = "result">The result code that caused this exception.</param>
+        public COMException(Result result) : this(ResultDescriptor.Find(result))
+        {
+            HResult = (int)result;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="COMException"/> class.
+        /// </summary>
+        /// <param name="descriptor">The result descriptor.</param>
+        public COMException(ResultDescriptor descriptor) : base(descriptor.ToString())
+        {
+            this.descriptor = descriptor;
+            HResult = (int)descriptor.Result;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="COMException"/> class.
+        /// </summary>
+        /// <param name="result">The error result code.</param>
+        /// <param name="message">The message describing the exception.</param>
+        public COMException(Result result, string message) : base(message)
+        {
+            this.descriptor = ResultDescriptor.Find(result);
+            HResult = (int)result;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="COMException"/> class.
+        /// </summary>
+        /// <param name="result">The error result code.</param>
+        /// <param name="message">The message describing the exception.</param>
+        /// <param name="args">formatting arguments</param>
+        public COMException(Result result, string message, params object[] args) : base(string.Format(CultureInfo.InvariantCulture, message, args))
+        {
+            this.descriptor = ResultDescriptor.Find(result);
+            HResult = (int)result;
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "COMException" /> class.
+        /// </summary>
+        /// <param name = "message">The message describing the exception.</param>
+        /// <param name="args">formatting arguments</param>
+        public COMException(string message, params object[] args) : this(Result.Fail, message, args)
+        {
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "COMException" /> class.
+        /// </summary>
+        /// <param name = "message">The message describing the exception.</param>
+        /// <param name = "innerException">The exception that caused this exception.</param>
+        /// <param name="args">formatting arguments</param>
+        public COMException(string message, Exception innerException, params object[] args)
+            : base(string.Format(CultureInfo.InvariantCulture, message, args), innerException)
+        {
+            this.descriptor = ResultDescriptor.Find(Result.Fail);
+            HResult = (int)Result.Fail;
+        }
+
+        /// <summary>
+        ///   Gets the <see cref = "Result">Result code</see> for the exception. This value indicates
+        ///   the specific type of failure that occurred within SharpDX.
+        /// </summary>
+        public Result ResultCode
+        {
+            get { return this.descriptor.Result; }
+        }
+
+        /// <summary>
+        ///   Gets the <see cref = "Result">Result code</see> for the exception. This value indicates
+        ///   the specific type of failure that occurred within SharpDX.
+        /// </summary>
+        public ResultDescriptor Descriptor
+        {
+            get { return this.descriptor; }
+        }
+    }
+}
---
+++ Native/Runtime/IUnknown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rc.Framework.Native.Runtime
+{
+    /// <summary>
+    /// Base interface for Component Object Model (COM).
+    /// </summary>
+    [Guid("00000000-0000-0000-C000-000000000046")]
+    public interface IUnknown
+    {
+        /// <summary>
+        /// Queries the supported COM interface on this instance.
+        /// </summary>
+        /// <param name="guid">The guid of the interface.</param>
+        /// <param name="comObject">The output COM object reference.</param>
+        /// <returns>If successful, <see cref="Result.Ok"/> </returns>
+        Result QueryInterface(ref Guid guid, out IntPtr comObject);
+
+        /// <summary>
+        /// Increments the reference count for an interface on this instance.
+        /// </summary>
+        /// <returns>The method returns the new reference count.</returns>
+        int AddReference();
+
+        /// <summary>
+        /// Decrements the reference count for an interface on this instance.
+        /// </summary>
+        /// <returns>The method returns the new reference count.</returns>
+        int Release();
+    }
+}
