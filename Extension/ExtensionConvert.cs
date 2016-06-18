73f61fe604b186cc45858fe3d726865c24840d64
AnotherAltr <kozyr@itsph.ru>
2016-06-18 13:24:28 +0300

    Отрефакторил ExtensionConvert.cs
    Убрал неймспейс, дабы при подлюкчении библиотеки, методы сразу были доступны в глобальном контексте.
    
    Так же, добавил метод конвертации DateTime в TimeSpan без проблем

--- Extension/ExtensionConvert.cs
+++ Extension/ExtensionConvert.cs
@@ -3,157 +3,124 @@
 // LicenseType="MIT"                    //                  Alise Wesp & Yuuki Wesp                     //
 // =====================================//==============================================================//
 using System;
-namespace RC.Framework.Extension
+using RC.Framework;
+/// <summary>
+/// Класс расширения конвертации
+/// </summary>
+public static class ExtensionConvert
 {
     /// <summary>
-    /// Класс расширения конвертации
+    /// Reads all available elements from an enumerator and inserts it into a collection.
     /// </summary>
-    public static class ExtensionConvert
+    /// <typeparam name="T">Type of the collection.</typeparam>
+    /// <param name="enumerator">The enumerator.</param>
+    /// <returns>List</returns>
+    public static RC.Framework.Collections.Generic.RList<T> ToList<T>(this System.Collections.IEnumerator enumerator)
     {
-        /// <summary>
-        /// Reads all available elements from an enumerator and inserts it into a collection.
-        /// </summary>
-        /// <typeparam name="T">Type of the collection.</typeparam>
-        /// <param name="enumerator">The enumerator.</param>
-        /// <returns>List</returns>
-        public static Collections.Generic.RList<T> ToList<T>(this System.Collections.IEnumerator enumerator)
+        RC.Framework.Collections.Generic.RList<T> collection = new RC.Framework.Collections.Generic.RList<T>();
+        while (enumerator.MoveNext())
+            collection.Add((T)enumerator.Current);
+        return collection;
+    }
+    /// <summary>
+    /// Конвертация строки в <see cref="Guid"/>
+    /// </summary>
+    /// <param name="t">строка содержащие <see cref="Guid"/> для преобразования.</param>
+    /// <returns>Преобразованное <see cref="Guid"/>.</returns>
+    public static Guid ToGUID(this string t) => new Guid(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="Int32"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static Int32 ToInt32(this string t) => Int32.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="Int64"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static Int64 ToInt64(this string t) => Int64.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="Int16"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static Int16 ToInt16(this string t) => Int16.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="UInt16"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static UInt16 ToUInt16(this string t) => UInt16.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="UInt32"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static UInt32 ToUInt32(this string t) => UInt32.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="UInt64"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static UInt64 ToUInt64(this string t) => UInt64.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="sbyte"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static sbyte ToSByte(this string t) => sbyte.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="byte"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static byte ToByte(this string t) => byte.Parse(t);
+
+    /// <summary>
+    /// Конвертация строки в <see cref="float"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static float ToSingle(this string t) => float.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <see cref="double"/>
+    /// </summary>
+    /// <param name="t">строка содержащие число для преобразования.</param>
+    /// <returns>Преобразованное число.</returns>
+    public static double ToDouble(this string t) => double.Parse(t);
+    /// <summary>
+    /// Конвертация строки в <typeparamref name="TEnum"/>
+    /// </summary>
+    /// <typeparam name="TEnum">строка содержащие значение для преобразования.</typeparam>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static TEnum ToEnum<TEnum>(this string t) => (TEnum)Enum.Parse(typeof(TEnum), t);
+    /// <summary>
+    ///  конвертация строки в  <typeparamref name="TEnum"/>
+    /// </summary>
+    /// <typeparam name="TEnum"></typeparam>
+    /// <param name="t"></param>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static bool TryToEnum<TEnum>(this string t, out TEnum obj)
+    {
+        //& При использовании Enum.TryParse происходит ошибка
+        //# Ошибка CS0453
+        //# Я хз как её исправить, не умею я генерики переводить
+        obj = default(TEnum);
+        try
         {
-            Collections.Generic.RList<T> collection = new Collections.Generic.RList<T>();
-            while (enumerator.MoveNext())
-                collection.Add((T)enumerator.Current);
-            return collection;
+            obj = (TEnum)Enum.Parse(typeof(TEnum), t);
+            return true;
         }
-        /// <summary>
-        /// Конвертация строки в <see cref="Guid"/>
-        /// </summary>
-        /// <param name="t">строка содержащие <see cref="Guid"/> для преобразования.</param>
-        /// <returns>Преобразованное <see cref="Guid"/>.</returns>
-        public static Guid ToGUID(this string t)
+        catch
         {
-            return new Guid(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="Int32"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static Int32 ToInt32(this string t)
-        {
-            return Int32.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="Int64"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static Int64 ToInt64(this string t)
-        {
-            return Int64.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="Int16"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static Int16 ToInt16(this string t)
-        {
-            return Int16.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="UInt16"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static UInt16 ToUInt16(this string t)
-        {
-            return UInt16.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="UInt32"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static UInt32 ToUInt32(this string t)
-        {
-            return UInt32.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="UInt64"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static UInt64 ToUInt64(this string t)
-        {
-            return UInt64.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="sbyte"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static sbyte ToSByte(this string t)
-        {
-            return sbyte.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="byte"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static byte ToByte(this string t)
-        {
-            return byte.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="float"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static float ToSingle(this string t)
-        {
-            return float.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <see cref="double"/>
-        /// </summary>
-        /// <param name="t">строка содержащие число для преобразования.</param>
-        /// <returns>Преобразованное число.</returns>
-        public static double ToDouble(this string t)
-        {
-            return double.Parse(t);
-        }
-        /// <summary>
-        /// Конвертация строки в <typeparamref name="TEnum"/>
-        /// </summary>
-        /// <typeparam name="TEnum">строка содержащие значение для преобразования.</typeparam>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        public static TEnum ToEnum<TEnum>(this string t)
-        {
-            return (TEnum)Enum.Parse(typeof(TEnum), t);
-        }
-        /// <summary>
-        ///  конвертация строки в  <typeparamref name="TEnum"/>
-        /// </summary>
-        /// <typeparam name="TEnum"></typeparam>
-        /// <param name="t"></param>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static bool TryToEnum<TEnum>(this string t, out TEnum obj)
-        {
-            //& При использовании Enum.TryParse происходит ошибка
-            //# Ошибка CS0453
-            //# Я хз как её исправить, не умею я генерики переводить
-            obj = default(TEnum);
-            try
-            {
-                obj = (TEnum)Enum.Parse(typeof(TEnum), t);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            return false;
         }
     }
+    /// <summary>
+    /// Конвертация типа <see cref="DateTime"/> в <see cref="TimeSpan"/>.
+    /// </summary>
+    public static TimeSpan ToTime(this DateTime s) => new TimeSpan(s.Ticks);
 }
